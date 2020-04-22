d6702d7ae0abc6c5843a53fff6e7956ec2cacf52
Darko Jovevski <platzmanfan9@gmail.com>
2020-04-21 19:07:27 -0700

    added placeholder for options to choose and added apiroutes to the js file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,9 @@ var data = require ("../data/friends.js");
 
 module.exports = function(app){
     app.get("/api/friends", function (req,res){
-        res.json(data)
+        res.json(data);
+    })
+    app.post("/api/firends", function (req,res){
+        res.json(data);
     })
 }
