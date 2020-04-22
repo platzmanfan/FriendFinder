ae4a7927dfba0ced0796b33c04ba3a334355a036
Darko Jovevski <platzmanfan9@gmail.com>
2020-04-22 15:20:47 -0700

    added more data in friends.js also fixed the image size to be inside the toggle

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsData = [
     {
         name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        photo:"https://lh3.googleusercontent.com/proxy/ngSVbfSKAcr1FFtiWz-6HfxZchzVzhtoqh-EAW9O40xaPyluDLVPPTHlS_a5XBEasGqOt_557GEGQ2uf_U6n9PtnfzbUdhQ",
         scores: [
             "5",
             "1",
@@ -47,6 +47,22 @@ var friendsData = [
             "5"
         ]
 
+    },
+    {
+        name: "Darko Jov",
+        photo:"https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/p960x960/84349274_10221410621284715_1553451226710933504_o.jpg?_nc_cat=102&_nc_sid=85a577&_nc_oc=AQlOZxAdmY-I4o9-GbL5JJiRqKJf4ZLUsyNwy5ub7HKxYOjlG7d0QU1vRil8G0s5l5mB9Bi1-kvsuyXaTsvk3DS3&_nc_ht=scontent-sjc3-1.xx&_nc_tp=6&oh=f3c17e5f3590e319860491c0b4392e36&oe=5EC5870A",
+        scores:[
+            "5",
+            "4",
+            "4",
+            "2",
+            "3",
+            "1",
+            "5",
+            "3",
+            "5",
+            "4"
+        ]
     }
 ];
 module.exports = friendsData;
